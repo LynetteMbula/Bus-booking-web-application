578d5ffccb658c019959520cfc66eb9a10f04687
Allen <allenhe77@gmail.com>
2020-09-29 17:05:38 +0300

    change booking state func

---
+++ booking.js
@@ -0,0 +1,13 @@
+const changeToSelected = (id) => {
+  document.getElementById(id).setAttribute("class", "selected");
+
+  // console.log("from changeColor")
+};
+/*
+const onBook = () => {
+  document.getElementById("seat1").addEventListener("click", changeToSelected);
+};
+
+onBook();
+
+*/

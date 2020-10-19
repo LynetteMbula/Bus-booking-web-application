e284910d8478c0f905bc3ef4c1905c8343b8390d
Allen <allenhe77@gmail.com>
2020-10-19 23:59:32 +0300

    routing done in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Route, Switch} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Route, Switch } from "react-router-dom";
 //importing a lirary to allow routing . Routing is  is the ability to move between different parts of an application when a user enters a URL or clicks an element ==> npm install react-router-dom to install the library  React-Dom works like a navigation system inside the project to navigate from one page to another.
 //if npm install react-router-dom does not workon vs code try closing vscode and running it in cmd
 
 //import logo from './logo.svg';
-import './App.css';
-import Home from './pages/home';
+import "./App.css";
+import Home from "./pages/Home";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Payment from "./components/Payment";
 
 function App() {
   return (
     <div className="wrapper">
-    <Header />
-    <article>
-     <Home />
-      <Footer />
-     </article>
-
+      {/* the components for the routes should be enclosed in <section> tag */}
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/makepayment">
+          <Payment />
+        </Route>
+      </Switch>
+      <article>
+        <Footer />
+      </article>
     </div>
-         
   );
 }
 
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,138 +1,121 @@
-
-import React, { Component } from 'react';
-import Header from "../components/header";
-import Footer from "../components/footer";
-
+import React, { Component } from "react";
 
 //this imports the header and footer to home.js
 
+class Home extends Component {
+  render() {
+    return (
+      <section id="busForm">
+        <div id="booking" class="section">
+          <div class="section-center">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="booking-cta">
+                    <h1>Book your Bus today</h1>
+                    <p>The easiest and fastest way to book your bus online!</p>
+                  </div>
+                </div>
+                <div class="col-md-7 col-md-offset-1">
+                  <div class="booking-form">
+                    <form>
+                      <div class="form-group">
+                        <div class="row">
+                          <div class="col-md-6">
+                            <div class="form-group">
+                              <span class="form-label">Leaving from</span>
+                              <select class="form-control">
+                                <option value=""></option>
+                                <option>Nairobi</option>
+                                <option>Mombasa</option>
+                                <option>Dar es salam</option>
+                                <option>Dodoma</option>
+                              </select>
+                              <span class="select-arrow"></span>
+                            </div>
+                          </div>
+                          <div class="col-md-6">
+                            <div class="form-group">
+                              <span class="form-label">Destination to</span>
+                              <select class="form-control">
+                                <option value=""></option>
+                                <option>Nairobi</option>
+                                <option>Mombasa</option>
+                                <option>Dar es salam</option>
+                                <option>Dodoma</option>
+                              </select>
+                              <span class="select-arrow"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-6">
+                            <div class="form-group">
+                              <span class="form-label">Travel Date</span>
 
-class home extends Component{
-    render(){
-        return(
-            
-            
-                     
-<section id="busForm">
-	<div id="booking" class="section">
-		<div class="section-center">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="booking-cta" >
-							<h1>Book your Bus today</h1>
-							<p>The easiest and fastest way to book your bus online!</p>
-						</div>
-					</div>
-					<div class="col-md-7 col-md-offset-1">
-						<div class="booking-form">
-							<form>
-								<div class="form-group">
-	
-								<div class="row">
-									<div class="col-md-6">
-										<div class="form-group">
-											<span class="form-label">Leaving from</span>
-											<select class="form-control">
-												<option value=""></option>
-												<option>Nairobi</option>
-												<option>Mombasa</option>
-												<option>Dar es salam</option>
-												<option>Dodoma</option>
-											</select>
-											<span class="select-arrow"></span>
-											
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<span class="form-label">Destination to</span>
-											<select class="form-control">
-												<option value=""></option>
-												<option>Nairobi</option>
-												<option>Mombasa</option>
-												<option>Dar es salam</option>
-												<option>Dodoma</option>
-											</select>
-											<span class="select-arrow"></span>
-											
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-6">
-										<div class="form-group">
-											<span class="form-label">Travel Date</span>
-	
-											<input class="form-control" type="date" required />
-										</div>
-									</div>
-									
-								</div>
-								<div class="row">
-									<div class="col-md-4">
-										<div class="form-group">
-											<span class="form-label">Adults (18+)</span>
-											<select class="form-control">
-												<option value=""></option>
-												<option>1</option>
-												<option>2</option>
-												<option>3</option>
-												<option>4</option>
-												<option>5</option>
-											</select>
-											<span class="select-arrow"></span>
-										</div>
-									</div>
-									<div class="col-md-4">
-										<div class="form-group">
-											<span class="form-label">Children (0-17)</span>
-											<select class="form-control">
-												<option value=""></option>
-												<option>0</option>
-												<option>1</option>
-												<option>2</option>
-												<option>3</option>
-												<option>4</option>
-												<option>5</option>
-											</select>
-											<span class="select-arrow"></span>
-										</div>
-									</div>
-									<div class="col-md-4">
-										<div class="form-group">
-											<span class="form-label">Travel class</span>
-											<select class="form-control">
-												<option value=""></option>
-												<option>Economy class</option>
-												<option>First class</option>
-											</select>
-											<span class="select-arrow"></span>
-										</div>
-									</div>
-								</div>
-								<div class="form-btn">
-									<button class="submit-btn">Book Now</button>
-                                </div>
-                                </div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-    </div> 
-         
-</section> 
-
-
-
-
-
-
-)
-        }
+                              <input
+                                class="form-control"
+                                type="date"
+                                required
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-4">
+                            <div class="form-group">
+                              <span class="form-label">Adults (18+)</span>
+                              <select class="form-control">
+                                <option value=""></option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                              </select>
+                              <span class="select-arrow"></span>
+                            </div>
+                          </div>
+                          <div class="col-md-4">
+                            <div class="form-group">
+                              <span class="form-label">Children (0-17)</span>
+                              <select class="form-control">
+                                <option value=""></option>
+                                <option>0</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                              </select>
+                              <span class="select-arrow"></span>
+                            </div>
+                          </div>
+                          <div class="col-md-4">
+                            <div class="form-group">
+                              <span class="form-label">Travel class</span>
+                              <select class="form-control">
+                                <option value=""></option>
+                                <option>Economy class</option>
+                                <option>First class</option>
+                              </select>
+                              <span class="select-arrow"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="form-btn">
+                          <button class="submit-btn">Book Now</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
-export default home;
-
-
+export default Home;
